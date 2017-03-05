316e2cfe76aa82d70e33a003c0129d56b15fad2a
Sébastien De Fauw <sdefauw@gmail.com>
2017-03-05 11:26:54 +0100

    Review source code structure of server selector crt

--- brorig/www/js/app/server-selector.controller.js
+++ brorig/www/js/app/server-selector.controller.js
@@ -8,62 +8,71 @@
 
     function ServerSelectorController($scope, sss, ps) {
 
-        this.serversFiltered = null;
+        var ssc = {
+            serversFiltered: null,
 
-        this.servers = function () {
-            if (this.serversFiltered == null)
-                return sss.servers;
-            return this.serversFiltered;
-        };
+            servers: function () {
+                if (ssc.serversFiltered == null)
+                    return sss.servers;
+                return ssc.serversFiltered;
+            },
 
-        this.display = function () {
-            return sss.display;
-        };
-        this.close = function () {
-            sss.display = false;
-        };
-        this.open = function () {
-            sss.display = true;
-        };
-        this.process = function () {
-            this.close();
-            ps.windowsManager.closeAll();
-            if (!sss.process())
-                this.open();
-        };
-        this.select = function (server) {
-            for (var i in sss.servers) {
-                if (sss.servers[i].key == server.key) {
-                    sss.servers[i].selected = !sss.servers[i].selected;
-                    if (sss.servers[i].selected)
-                        sss.storage.addServerkey(server.key);
-                    else
-                        sss.storage.rmServerkey(server.key);
+            display: function () {
+                return sss.display;
+            },
+
+            close: function () {
+                sss.display = false;
+            },
+
+            open: function () {
+                sss.display = true;
+            },
+
+            process: function () {
+                ssc.close();
+                ps.windowsManager.closeAll();
+                if (!sss.process())
+                    ssc.open();
+            },
+
+            select: function (server) {
+                for (var i in sss.servers) {
+                    if (sss.servers[i].key == server.key) {
+                        sss.servers[i].selected = !sss.servers[i].selected;
+                        if (sss.servers[i].selected)
+                            sss.storage.addServerkey(server.key);
+                        else
+                            sss.storage.rmServerkey(server.key);
+                        return;
+                    }
+                }
+            },
+
+            search: function () {
+                var keyword = $scope.searchkeys;
+                if (!keyword || keyword == '') {
+                    ssc.serversFiltered = sss.servers;
                     return;
                 }
+                var keywords = keyword.split(" ");
+                var list = [];
+                sss.servers.forEach(function (item) {
+                    var s = item.key + " " + item.name + " " + item.group;
+                    var d = false;
+                    keywords.forEach(function (keyword) {
+                        var re = new RegExp(keyword, "i");
+                        if (!re.test(s))
+                            d = true;
+                    });
+                    if (!d)
+                        list.push(item);
+                });
+                ssc.serversFiltered = list;
             }
+
         };
 
-        this.search = function () {
-            var keyword = $scope.searchkeys;
-            if (!keyword || keyword == '') {
-                this.serversFiltered = sss.servers;
-                return;
-            }
-            var keywords = keyword.split(" ");
-            var list = [];
-            sss.servers.forEach(function (item) {
-                var s = item.key + " " + item.name + " " + item.group;
-                var d = false;
-                keywords.forEach(function (keyword) {
-                    var re = new RegExp(keyword, "i");
-                    if (!re.test(s))
-                        d = true;
-                });
-                if (!d)
-                    list.push(item);
-            });
-            this.serversFiltered = list;
-        };
+        return ssc;
     }
 })();
